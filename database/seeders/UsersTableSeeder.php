0abd72980166a24fac7bcc2a8a5281bc95d0b964
mrizkimaulidan <mrizkimaulidan@hotmail.com>
2020-10-12 18:57:49 +0800

    set up UsersTableSeeder

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -1,9 +1,11 @@
 <?php
+
 namespace Database\Seeders;
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Faker\Factory as Faker;
 
 class UsersTableSeeder extends Seeder
 {
@@ -14,13 +16,26 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        $faker = Faker::create('id_ID');
+
         DB::table('users')->insert([
-            'name' => 'Admin Admin',
-            'email' => 'admin@argon.com',
+            'name' => 'Administrator',
+            'email' => 'admin@mail.com',
             'email_verified_at' => now(),
             'password' => Hash::make('secret'),
             'created_at' => now(),
             'updated_at' => now()
         ]);
+
+        for ($i = 1; $i <= 20; $i++) {
+            DB::table('users')->insert([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'email_verified_at' => now(),
+                'password' => Hash::make('secret'),
+                'created_at' => now(),
+                'updated_at' => now()
+            ]);
+        }
     }
 }
