486394d8759fe53c1d7420c2b4669e1d4f3f5753
mrizkimaulidan <mrizkimaulidan@hotmail.com>
2020-10-14 07:27:17 +0800

    set up users seeder

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -17,28 +19,22 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $faker = Faker::create('id_ID');
-
         DB::table('users')->insert([
             'name' => 'Administrator',
+            'gender' => mt_rand(1, 2),
             'email' => 'admin@mail.com',
+            'phone_number' => '081346153182',
+            'join_date' => Carbon::createFromDate(mt_rand(1950, 2020), mt_rand(1, 12), mt_rand(1, 31)),
+            'date_of_birth' => Carbon::createFromDate(mt_rand(1950, 2020), mt_rand(1, 12), mt_rand(1, 31)),
             'email_verified_at' => now(),
             'password' => Hash::make('secret'),
+            'address' => 'Jalan Rapak Indah',
+            'job' => 'Pegawai Swasta',
             'remember_token' => Str::random(10),
             'created_at' => now(),
             'updated_at' => now()
         ]);
 
-        for ($i = 1; $i <= 20; $i++) {
-            DB::table('users')->insert([
-                'name' => $faker->name,
-                'email' => $faker->email,
-                'email_verified_at' => now(),
-                'password' => Hash::make('secret'),
-                'remember_token' => Str::random(10),
-                'created_at' => now(),
-                'updated_at' => now()
-            ]);
-        }
+        User::factory()->count(29)->create();
     }
 }
