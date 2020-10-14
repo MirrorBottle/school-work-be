9842f507b7f423d5ff86307622112e0d73b737b9
MirrorBottle <setiawanbayu66152@gmail.com>
2020-10-14 23:51:17 +0800

    fix for windows

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Schema\Builder;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        config(['app.locale' => 'id']);
+        \Carbon\Carbon::setLocale('id');
+        Schema::defaultStringLength(191);
+        date_default_timezone_set('Asia/Makassar');
     }
 }
