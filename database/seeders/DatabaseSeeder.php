59b552b0bd2bd4cc6741b71336e89624fdbce642
mrizkimaulidan <mrizkimaulidan@hotmail.com>
2020-10-14 22:18:41 +0800

    roles seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,6 @@ class DatabaseSeeder extends Seeder
         $this->call([LoansTableSeeder::class]);
         $this->call([PaymentsTableSeeder::class]);
         $this->call([DepositsTableSeeder::class]);
+        $this->call([RolesTableSeeder::class]);
     }
 }
