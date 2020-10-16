e2968b27950634a0dba670ef4457cb382f1e8421
mrizkimaulidan <mrizkimaulidan@hotmail.com>
2020-10-17 01:03:37 +0800

    added balance migration and model

---
+++ database/migrations/2020_10_17_005601_create_balances_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBalancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('balances', function (Blueprint $table) {
+            $table->id();
+            $table->integer('balance');
+            $table->morphs('balanceable');
+            $table->dateTime('changed_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('balances');
+    }
+}
